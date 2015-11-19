a2e392d36f5b2e369f0f71f4bb6aff3d04345706
Joanne Robert <joanne@brandwatch.com>
2015-11-19 15:59:57 +0000

    Chat functional with greeting on connection

--- index.js
+++ index.js
@@ -6,23 +6,25 @@ app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
 });
 
+// 'io' is the client side
+// 'socket' is a channel transporting events/communications
 io.on('connection', function(socket){
 	console.log('A user just connected.');
 
+	// Emit to sender only
+	socket.emit('chat_message', 'Oh, hi there!');
+
 	socket.on('chat_message', function(msg){
 		console.log('Message: ' + msg);
+
+		// Emit to all clients
+		io.emit('chat_message', msg);
 	});
 
 	socket.on('disconnect', function(){
 		console.log('User just disconnected.');
 	});
 });
-
-// io.on('connection', function(socket){
-//   socket.on('chat message', function(msg){
-//     console.log('message: ' + msg);
-//   });
-// });
 
 http.listen(5000, function(){
 	console.log('Listening on *:5000');
